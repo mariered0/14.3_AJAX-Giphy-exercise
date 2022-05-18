c83fa2f990b5229be42794769ff042782ac1f391
mariered0 <98713565+mariered0@users.noreply.github.com>
2022-05-18 15:07:48 -0700

    add css

--- app.js
+++ app.js
@@ -4,53 +4,43 @@ const input = document.querySelector('#search-input');
 const imgDiv = document.querySelector('#img-container');
 const removeBtn = document.querySelector('#remove-btn');
 const $allImages = $('img');
-// const allImages = document.querySelectorAll('img');
-//this is global variable, but cannot have the imgUrl. it becomes undefined
 
-async function getGiphy(api_key, q){
-    try{
+async function getGiphy(api_key, q) {
+    try {
         const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${q}&limit=1`);
         const img = document.createElement('img');
         img.src = res.data.data[0].images.downsized.url;
         imgDiv.appendChild(img);
-    }catch (e){
+    } catch (e) {
         console.log(e);
     };
 };
 
-removeBtn.addEventListener('click', function(e){
+removeBtn.addEventListener('click', function (e) {
     const $allImages = $('img');
     $allImages.remove();
 })
 
-//removeBtn with plain JavaScript:
-// removeBtn.addEventListener('click', function(e){
-//     allImages.forEach(function(image) {
-//         image.remove();
-//     });
-// })
-
-//How do i access imgUrl from a separate function?
-
-// function createBtn(){
-//     removeBtn.innerText = 'x';
-//     imgDiv.appendChild(removeBtn);
-//     removeBtn.addEventListener('click', function(e){
-//         img.remove();
-//         removeBtn.remove();
-//     })
-// };
-
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
     e.preventDefault();
     getGiphy(api_key, input.value);
-    // createImage();
     input.value = '';
 });
 
-// function createImage(){
-//     const img = document.createElement('img');
-//     console.log('in createImage func', imgUrl);
-//     img.src = imgUrl;
-//     imgDiv.appendChild(img);
-// };
+
+
+function randomRGB() {
+    //this will give a whole number instead of a dicimal.
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const letters = document.querySelectorAll('.letter');
+
+const intervalId = setInterval(function () {
+    for (let letter of letters) {
+        letter.style.color = randomRGB();
+    }
+}, 1000);
