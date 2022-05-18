a1d261a5b704300a6df2ba4a68e18cfb226bb56b
mariered0 <98713565+mariered0@users.noreply.github.com>
2022-05-18 08:00:38 -0700

    use jQuery for removing all images

--- app.js
+++ app.js
@@ -2,28 +2,24 @@ const api_key = 'T92FH9Vwx2A3pGpt1O08fUErbq2lri9i';
 const form = document.querySelector('#form');
 const input = document.querySelector('#search-input');
 const imgDiv = document.querySelector('#img-container');
-// const removeBtn = document.querySelector('#remove-btn');
+const removeBtn = document.querySelector('#remove-btn');
+const $allImages = $('img');
 // const allImages = document.querySelectorAll('img');
-
 //this is global variable, but cannot have the imgUrl. it becomes undefined
 
 async function getGiphy(api_key, q){
     const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${q}&limit=1`);
-    
     const img = document.createElement('img');
-    const removeBtn = document.querySelector('#remove-btn');
-    const allImages = document.querySelectorAll('img');
-
     img.src = res.data.data[0].images.downsized.url;
     imgDiv.appendChild(img);
-
-    removeBtn.addEventListener('click', function(e){
-        allImages.forEach(function(img) {
-            img.remove();
-        });
-    })
 };
 
+removeBtn.addEventListener('click', function(e){
+    const $allImages = $('img');
+    $allImages.remove();
+})
+
+//removeBtn with plain JavaScript:
 // removeBtn.addEventListener('click', function(e){
 //     allImages.forEach(function(image) {
 //         image.remove();
